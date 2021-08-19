7e63cd6ee3d2c3863ab9f9df36a7796dd4524a20
nitishgiri007 <85404432+nitishgiri007@users.noreply.github.com>
2021-08-19 06:53:42 -0700

    Update Hero.js

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -129,21 +129,21 @@ const Hero = ({ slides }) => {
  const length =slides.length
  const timeout = useRef(null)
 
-//  useEffect(() => {
-//      const nextSlide = () =>{
-//         setCurrent(current => (current === length - 1 ? 0 :current +1))
-//      };
+ useEffect(() => {
+     const nextSlide = () =>{
+        setCurrent(current => (current === length - 1 ? 0 :current +1))
+     };
 
-//      timeout.current = setTimeout(nextSlide, 1000);
+     timeout.current = setTimeout(nextSlide, 5000);
 
-//      return function(){
-//          if(timeout.current){
-//              clearTimeout(timeout.current);
-//          }
-//         };
-//      },
-//     [current, length]
-//  );
+     return function(){
+         if(timeout.current){
+             clearTimeout(timeout.current);
+         }
+        };
+     },
+    [current, length]
+ );
 
  const nextSlide = () => {
     if(timeout.current){
